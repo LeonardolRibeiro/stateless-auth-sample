284fd0f19fa60d2712a1557aefa5446fb722faf3
leonardo.laia <leonardo.ribeiro@inteligate.com.br>
2024-02-06 14:42:56 -0300

    reestruturação

--- stateless-any-api/src/main/java/br/com/microservice/statelessanyapi/infra/exception/ExceptionDetails.java
+++
@@ -1,4 +0,0 @@
-package br.com.microservice.statelessanyapi.infra.exception;
-
-public record ExceptionDetails(int status, String message){
-}
--- stateless-auth-api/src/main/java/br/com/microservice/statelessauthapi/infra/exception/ExceptionDetails.java
+++
@@ -1,4 +0,0 @@
-package br.com.microservice.statelessauthapi.infra.exception;
-
-public record ExceptionDetails(int status, String message){
-}
